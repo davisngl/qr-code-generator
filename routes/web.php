0e61d953a91bf8a5c699567efa70b9498db9b4a8
Dāvis Naglis <davis.naglis@tapbox.eu>
2022-09-21 20:36:22 +0300

    Add blank page as main page

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,8 @@
 use App\Http\Controllers\QrCodeController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return "Nothing to see here.";
+});
+
 Route::get('/qr-code', QrCodeController::class);
